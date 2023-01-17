e31d69ebd8d24c9adfb3654c1e79b179bb8050e2
Suhan Kacholia <suhan.kacholia03@gmail.com>
2023-01-17 14:38:35 -0500

    init

--- pages/Summarizer.py
+++ pages/Summarizer.py
@@ -1,8 +1,16 @@
 import streamlit as st
+import openai
 
 st.set_page_config(
-    page_title="Summarizer",
-    page_icon="👋",
+    page_title="Summarizer"
 )
 
-st.write("# Welcome to Streamlit!2 👋")
+st.write("# Summarize Text")
+
+text = st.text_area(label="Text", height=300, max_chars=4000)
+
+if st.button('Write'):
+    prompt = "Summarize the main points from the article in less than 120 characters: "
+    response = openai.complete(prompt = prompt + text)
+    st.write(response['choices'][0]['text'])
+

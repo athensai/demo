9dc39739da902b1970c805eb16d026687658b494
athensai <122765545+athensai@users.noreply.github.com>
2023-01-17 12:11:38 -0500

    Update grant_writing.py

--- pages/grant_writing.py
+++ pages/grant_writing.py
@@ -1,8 +1,18 @@
 import streamlit as st
+import openai
 
 st.set_page_config(
     page_title="Grant Writing",
-    page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit!6 👋")
+openai.api_key = st.secrets["openaiKey"]
+st.write("# Welcome to Grant Writer!")
+
+org = st.text_input("Organization name: ", disabled=False)
+amount = st.text_input("Amount requested: ", disabled=False)
+project = st.text_input("Project description: ", disabled=False)
+
+prompt = "Write a grant proposal for " + org + ". They are requesting " + amount + "for " + project
+
+response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=150)
+st.write(response['choices'][0]['text'])
